991bfc077c0d8ab9f45f77a0cede8e3110095576
pukim <pukim@hanmail.net>
2015-09-21 16:29:26 +0900

    my first android webview

---
+++ MyWebViewApp/src/com/example/mywebviewapp/MainActivity.java
@@ -0,0 +1,59 @@
+/*
+ * https://developer.chrome.com/multidevice/webview/gettingstarted
+ */
+package com.example.mywebviewapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends ActionBarActivity {
+	private WebView mWebView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mWebView = (WebView)findViewById(R.id.activity_main_webview);
+		
+		// Enable javascript
+		WebSettings webSettings = mWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		
+		//mWebView.loadUrl("http://m.daum.net");
+		mWebView.loadUrl("file:///android_asset/www/html/hello.html");
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
